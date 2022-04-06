0d4b16c95f282d202f23c4d39766d0074c1793d6
darttusin <ivanchepikov444@gmail.com>
2022-04-06 05:58:15 -0700

    db

--- database/db.py
+++ database/db.py
@@ -3,7 +3,7 @@ import asyncpg
 SQL_SELECT = 'SELECT schedule FROM schedule_table WHERE group={} AND dayofweek = {} AND even={}'
 
 async def get_schedule_from_database(group: str, day: str, even: bool):
-    conn = await asyncpg.connect(user='postgres', password='admin', database='schedules', host='localhost')
+    conn = await asyncpg.connect(user='postgres', password='postgres', database='schedules', host='localhost')
     schedule = await conn.fetch(SQL_SELECT.format(group.lower(), day.lower(), even))
     await conn.close()
     return schedule
