83bbe593142506db453f2a236e7c8d4460af66c9
3xiced <3xiced@gmail.com>
2022-04-07 15:51:40 +0300

    Как думаешь кто виноват???

--- utils/constants.py
+++ utils/constants.py
@@ -10,8 +10,8 @@ PASSWORD = os.environ.get('DBPASSWORD')
 NAME = os.environ.get('DBNAME')
 HOST = os.environ.get('DBHOST')
 
-SQL_SELECT_SCHEDULE = "SELECT schedule FROM schedule_table WHERE group='{}' AND dayofweek = '{}' AND even='{}'"
-SQL_SELECT_FULL_SCHEDULE = 'SELECT schedule FROM schedule_table WHERE group={} AND even={}'
+SQL_SELECT_SCHEDULE = "SELECT schedule FROM schedule_table WHERE streamgroup='{}' AND dayofweek = '{}' AND even='{}'"
+#SQL_SELECT_FULL_SCHEDULE = 'SELECT schedule FROM schedule_table WHERE group={} AND even={}' ты блять сначала научись первый запрос нормально писать а потом блять в полное раписание лезь
 
 if USER == None or PASSWORD == None or NAME == None or HOST == None:
     raise NoneException
