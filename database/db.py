82405be769de3268cdcdb799cbdc18bd5e64ae5a
3xiced <3xiced@gmail.com>
2022-04-07 15:04:06 +0300

    Фикс краша бд

--- database/db.py
+++ database/db.py
@@ -1,22 +1,6 @@
 import asyncpg
-import os
-
-
-class NoneException(Exception):
-    pass
-
-
-USER = os.environ.get('DBUSER')
-PASSWORD = os.environ.get('DBPASSWORD')
-NAME = os.environ.get('DBNAME')
-HOST = os.environ.get('DBHOST')
-
-SQL_SELECT_SCHEDULE = 'SELECT schedule FROM schedule_table WHERE group={} AND dayofweek = {} AND even={}'
-SQL_SELECT_FULL_SCHEDULE = 'SELECT schedule FROM schedule_table WHERE group={} AND even={}'
-
-if USER == None or PASSWORD == None or NAME == None or HOST == None:
-    raise NoneException
-
+from utils.constants import SQL_SELECT_SCHEDULE
+from utils.constants import USER, PASSWORD, HOST, NAME
 
 async def db_connect(user: str, password: str, name: str, host: str):
     conn = await asyncpg.connect(user=user, password=password, database=name, host=host)
@@ -24,7 +8,7 @@ async def db_connect(user: str, password: str, name: str, host: str):
 
 
 async def get_schedule_from_database(group: str, day: str, even: bool):
-    conn = await db_connect()
+    conn = await db_connect(USER, PASSWORD, HOST, NAME)
     schedule = await conn.fetch(SQL_SELECT_SCHEDULE.format(group.lower(), day.lower(), even))
     await conn.close()
     return schedule
