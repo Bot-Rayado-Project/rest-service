c4f74b3156fb8f376c7f7a07388be9400d8a82bb
3xiced <3xiced@gmail.com>
2022-04-07 14:58:53 +0300

    Теперь обязательно задание локальных переменный для подключения к БД

--- database/db.py
+++ database/db.py
@@ -1,10 +1,25 @@
 import asyncpg
+import os
+
+
+class NoneException(Exception):
+    pass
+
+
+USER = os.environ.get('DBUSER')
+PASSWORD = os.environ.get('DBPASSWORD')
+NAME = os.environ.get('DBNAME')
+HOST = os.environ.get('DBHOST')
 
 SQL_SELECT_SCHEDULE = 'SELECT schedule FROM schedule_table WHERE group={} AND dayofweek = {} AND even={}'
 SQL_SELECT_FULL_SCHEDULE = 'SELECT schedule FROM schedule_table WHERE group={} AND even={}'
 
-async def db_connect():
-    conn = await asyncpg.connect(user='postgres', password='postgres', database='schedules', host='localhost')
+if USER == None or PASSWORD == None or NAME == None or HOST == None:
+    raise NoneException
+
+
+async def db_connect(user: str, password: str, name: str, host: str):
+    conn = await asyncpg.connect(user=user, password=password, database=name, host=host)
     return conn
 
 
