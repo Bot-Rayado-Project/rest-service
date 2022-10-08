39b7b47719390998f4a121194a5cf2d516202385
3xiced <3xiced@gmail.com>
2022-10-08 13:53:16 +0300

    Revert last commit

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ app = FastAPI()
 @app.get('/schedule/')
 async def get_schedule(id: int, stream_group: str, parity: str, day: str | None = None) -> dict:
     '''GET запрос на получение расписания по группе, четности недели и дню недели'''
+    stream_group = translit(stream_group, language_code='ru', reversed=True) if detect_language(
+        stream_group) is not None else stream_group
     day = DAYS_MATCHING.get(day) if day not in DAYS_ENG else day
     parity = PARITY_MATCHING.get(parity) if parity not in PARITY_RU else parity
     schedule_dict: dict = await db_get_schedule(id=id, stream_group=stream_group, parity=parity, day=day)
@@ -23,7 +25,8 @@ async def get_schedule(id: int, stream_group: str, parity: str, day: str | None 
 @app.post('/change-schedule-headman/')
 async def change_schedule_headman(request: ChangeScheduleHeadman) -> dict:
     '''POST запрос на запись изменений в расписании старосты'''
-    stream_group = request.stream_group
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
@@ -35,7 +38,8 @@ async def change_schedule_headman(request: ChangeScheduleHeadman) -> dict:
 @app.post('/change-schedule-personal/')
 async def change_schedule_personal(request: ChangeSchedulePersonal) -> dict:
     '''POST запрос на запись изменений в расписании персональном'''
-    stream_group = request.stream_group
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
@@ -47,7 +51,8 @@ async def change_schedule_personal(request: ChangeSchedulePersonal) -> dict:
 @app.post('/remove-pair-headman/')
 async def remove_pair_headman(request: RemovePairHeadman) -> dict:
     '''POST запрос на удаление пары из расписания'''
-    stream_group = request.stream_group
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
@@ -59,8 +64,8 @@ async def remove_pair_headman(request: RemovePairHeadman) -> dict:
 @app.post('/remove-pair-personal/')
 async def remove_pair_personal(request: RemovePairPersonal) -> dict:
     '''POST запрос на удаление пары из расписания'''
-    stream_group = request.stream_group
-
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
@@ -72,8 +77,8 @@ async def remove_pair_personal(request: RemovePairPersonal) -> dict:
 @app.post('/reset-schedule-headman/')
 async def reset_schedule_headman(request: ResetScheduleHeadman) -> dict:
     '''POST запрос на сброс измененного расписания в первоначальное состояние'''
-    stream_group = request.stream_group
-
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
@@ -85,8 +90,8 @@ async def reset_schedule_headman(request: ResetScheduleHeadman) -> dict:
 @app.post('/reset-schedule-personal/')
 async def reset_schedule_personal(request: ResetSchedulePersonal) -> dict:
     '''POST запрос на сброс измененного расписания в первоначальное состояние'''
-    stream_group = request.stream_group
-
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
@@ -98,8 +103,8 @@ async def reset_schedule_personal(request: ResetSchedulePersonal) -> dict:
 @app.post('/add-annotation-headman/')
 async def add_annotation_headman(request: AddAnnotationHeadman) -> dict:
     '''POST запрос на запись аннтоации ко дню определенной группы и четности недели'''
-    stream_group = request.stream_group
-
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
@@ -111,8 +116,8 @@ async def add_annotation_headman(request: AddAnnotationHeadman) -> dict:
 @app.post('/add-annotation-personal/')
 async def add_annotation_personal(request: AddAnnotationPersonal) -> dict:
     '''POST запрос на запись аннтоации ко дню определенной группы и четности недели'''
-    stream_group = request.stream_group
-
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
@@ -124,8 +129,8 @@ async def add_annotation_personal(request: AddAnnotationPersonal) -> dict:
 @app.post('/remove-annotation-headman/')
 async def remove_annotation_headman(request: RemoveAnnotationHeadman) -> dict:
     '''POST запрос на сброс измененного расписания в первоначальное состояние'''
-    stream_group = request.stream_group
-
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
@@ -137,8 +142,8 @@ async def remove_annotation_headman(request: RemoveAnnotationHeadman) -> dict:
 @app.post('/remove-annotation-personal/')
 async def remove_annotation_personal(request: RemoveAnnotationPersonal) -> dict:
     '''POST запрос на сброс измененного расписания в первоначальное состояние'''
-    stream_group = request.stream_group
-
+    stream_group = translit(request.stream_group, language_code='ru', reversed=True) if detect_language(
+        request.stream_group) is not None else request.stream_group
     day = DAYS_MATCHING.get(
         request.day) if request.day not in DAYS_ENG else request.day
     parity = PARITY_MATCHING.get(
